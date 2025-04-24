ad695f9f6dd7caf95db42d870b47403d262de702
Jake Ledner <jake.ledner@sphere.tech>
2025-04-24 00:22:15 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Jake Ledner on 4/24/25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
